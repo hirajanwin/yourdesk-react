04f9b5e3051baf71d3ae076ba5755446ea7e0513
Jonathan Cai <caimjonathan@gmail.com>
2020-07-04 13:43:28 -0700

    fix user profile pic circle, and remove form label from product search

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -14,7 +14,6 @@ const Profile = () => {
     <div className="body">
         <Image src={user.picture} style={{width: "200px", height: "200px"}} alt="Profile"/>
         <h2>{user.name}</h2>
-        <p>For transparency, this is all the information stored by Auth0 <strong>(other users cannot view this)</strong>:</p>
         {Object.keys(user).map((k, i) => <p key={i}>{k + ": " + user[k]}</p>)}
     </div>
   );
