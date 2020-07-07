139434aa4c50cb52af84e3b55512badf2f8fb4fa
Jonathan Cai <caimjonathan@gmail.com>
2020-07-07 00:59:20 -0700

    new homepage 💖

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,7 @@
-import React, {useState} from 'react';
-import deskImg from '../../assets/desk2.png';
+import React from 'react';
+import homeImg from '../../assets/home.png';
 import DeskCard from '../../components/DeskCard/DeskCard';
-// import DeskComponent from '../../components/DeskComponent/DeskComponent';
 import ProductCard from '../../components/ProductCard/ProductCard';
-import { Alert } from 'react-bootstrap';
-import Fade from '../../components/Util/Fade';
 import { GET_DESKS, GET_PRODUCTS } from '../../util/api';
 import { useQuery } from '@apollo/react-hooks';
 import './Home.css'
@@ -14,16 +11,11 @@ export default function Home() {
     const { data } = useQuery(GET_PRODUCTS);
     let featuredDesks = dataDesks ? dataDesks.deskMany.slice(0, 4) : [];
     let featuredProducts = data ? data.productMany.slice(0, 3) : [];
-    const [showAlert, setShowAlert] = useState(true);
 
     return (
-        <div className="body">
-                {<Fade in={showAlert} component={<Alert onClick={() => setShowAlert(false)} variant={'warning'} dismissible>Warning, this site is still under heavy construction <span role="img">🚧👷‍♂️🚧</span></Alert>} />}
+        <div className="home-body">
             <div className="Home">
-                <div className="SplashText">
-                    <h1 className="HomeTitle">If you spend a lot of time on your desk, make sure you love it!</h1>
-                </div>
-                <img className="DeskImage" alt="" src={deskImg}/>
+                <img className="DeskImage" alt="" src={homeImg}/>
                 <h3 className="HomeSubtitle">Check out the desks other people work, game, and study on.</h3>
                 <div className="FeaturedList">
                     {featuredDesks.map((desk, i) => <DeskCard key={i} desk={desk}/>)}
