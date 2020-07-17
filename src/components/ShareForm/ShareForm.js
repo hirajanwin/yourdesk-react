96b0ab0bb3011e85c4d07f6ff71ad3cadd1f67f0
Jonathan Cai <caimjonathan@gmail.com>
2020-07-16 23:27:31 -0700

    hanlding some edge cases with sharing desks

--- src/components/ShareForm/ShareForm.js
+++ src/components/ShareForm/ShareForm.js
@@ -10,7 +10,7 @@ export default function ShareForm(props) {
     const { isAuthenticated, loginWithPopup, user } = useAuth0();
     const deskProducts = useSelector(store => store.deskProducts);
     const dispatch = useDispatch();
-    const { onSuccessfulUpload } = props; 
+    const { onSuccessfulUpload } = props;
     const [isLoading, setLoading] = useState(false);
 
     const [createDeskProducts] = useMutation(CREATE_DESK_PRODUCTS);
@@ -25,20 +25,27 @@ export default function ShareForm(props) {
             return;
         }
         setLoading(true);
-        
+
+
         // Extract share form properties
         const form = event.currentTarget;
-        let properties = {};
+        let properties = {
+            favorite: "None",
+            name: user ? user.name + "'s Desk" : "New User's Desk",
+            use: "None",
+        };
         for (let i = 0; i < form.elements.length; i++) {
-          if (form.elements[i].id) {
-            properties[form.elements[i].id] = form.elements[i].value;
-          }
+            if (form.elements[i].id && form.elements[i].value) {
+                properties[form.elements[i].id] = form.elements[i].value;
+            }
         }
+
+        console.log(properties);
 
         // Construct clean DeskProduct objects for GraphQL mutation
         let cleanDeskProducts = Object.values(deskProducts.byIds).map(
             x => {
-                let {deskProduct:dp} = x;
+                let { deskProduct: dp } = x;
                 return {
                     product: dp.product._id,
                     coordX: dp.coordX,
@@ -48,51 +55,82 @@ export default function ShareForm(props) {
                 }
             }
         )
-    
+
         // Upload the image first
         uploadImage(props.image.file).then((resp) => {
             let url = resp.data.url;
             // Then create desk products
-            createDeskProducts({
-                variables: {
-                    newDeskProducts: cleanDeskProducts
-                }
-            }).then(
-                ({ error, data })=> {
-                    if (error) {
-                        console.log("Failed to create desk products!");
-                        return;
-                    } else {
-                        var desk = {
-                            desk_products: data.deskProductCreateMany.recordIds,
-                            ...properties,
-                            user: user.sub,
-                            img: url,
-                            date_created: new Date(),
-                        }
-                        console.log(desk);
-                        // Finally create the desk object
-                        createDesk({
-                            variables: {
-                                newDesk: desk
-                            }
-                        }).then(
-                            ({error}) => {
-                                if (error) {
-                                    console.log("Error creating desk!");
-                                    console.log(error);
-                                } else {
-                                    console.log("Creating desk was a success!");
-                                    
-                                    // Cleanup after yourself
-                                    onSuccessfulUpload();
-                                    dispatch(clearAllDeskProducts());
-                                }
-                            }
-                        )
+            if (cleanDeskProducts && cleanDeskProducts.length > 0) {
+                createDeskProducts({
+                    variables: {
+                        newDeskProducts: cleanDeskProducts
                     }
+                }).then(
+                    ({ error, data }) => {
+                        if (error) {
+                            console.log("Failed to create desk products!");
+                            return;
+                        } else {
+                            var desk = {
+                                desk_products: data.deskProductCreateMany.recordIds,
+                                ...properties,
+                                user: user.sub,
+                                img: url,
+                                date_created: new Date(),
+                            }
+                            console.log(desk);
+                            // Finally create the desk object
+                            createDesk({
+                                variables: {
+                                    newDesk: desk
+                                }
+                            }).then(
+                                ({ error }) => {
+                                    if (error) {
+                                        console.log("Error creating desk!");
+                                        console.log(error);
+                                    } else {
+                                        console.log("Creating desk was a success!");
+
+                                        // Cleanup after yourself
+                                        onSuccessfulUpload();
+                                        dispatch(clearAllDeskProducts());
+                                    }
+                                }
+                            )
+                        }
+                    }
+                )
+            } else {
+                var desk = {
+                    desk_products: [],
+                    ...properties,
+                    user: user.sub,
+                    img: url,
+                    date_created: new Date(),
                 }
-            )
+                console.log(desk);
+                // Finally create the desk object
+                createDesk({
+                    variables: {
+                        newDesk: desk
+                    }
+                }).then(
+                    ({ error }) => {
+                        if (error) {
+                            console.log("Error creating desk!");
+                            console.log(error);
+                        } else {
+                            console.log("Creating desk was a success!");
+
+                            // Cleanup after yourself
+                            onSuccessfulUpload();
+                            dispatch(clearAllDeskProducts());
+                        }
+                    }
+                )
+            }
+
         }).catch(function (error) {
             console.log("Error uploading image...");
             console.log(error);
@@ -103,21 +141,21 @@ export default function ShareForm(props) {
         <Form className={props.show ? "MainForm" : "hidden"} onSubmit={handleSubmit}>
             <Form.Group controlId="name">
                 <Form.Label>Give this desk a name!</Form.Label>
-                <Form.Control type="text" placeholder="The name of my cool desk!"/>
-            </Form.Group>
-            
-            <Form.Group controlId="use">
-                <Form.Label>What do you use this desk for?</Form.Label>
-                <Form.Control as="textarea" rows="3" placeholder="WFH, Gaming, Programming..."/>
-            </Form.Group>
-            
-            <Form.Group controlId="favorite">
-                <Form.Label>Which is your favorite product?</Form.Label>
-                <Form.Control as="textarea" rows="3" placeholder="My MacBook!"/>
+                <Form.Control type="text" placeholder="The name of my cool desk!" />
             </Form.Group>
 
-            <Button loading={isLoading.toString()} variant="primary" type="submit" disabled={isLoading || !props.image}> 
-            {isLoading ? 'Uploading...' : 'Share!'}
+            <Form.Group controlId="use">
+                <Form.Label>What do you use this desk for?</Form.Label>
+                <Form.Control as="textarea" rows="3" placeholder="WFH, Gaming, Programming..." />
+            </Form.Group>
+
+            <Form.Group controlId="favorite">
+                <Form.Label>Which is your favorite product?</Form.Label>
+                <Form.Control as="textarea" rows="3" placeholder="My MacBook!" />
+            </Form.Group>
+
+            <Button loading={isLoading.toString()} variant="primary" type="submit" disabled={isLoading || !props.image}>
+                {isLoading ? 'Uploading...' : 'Share!'}
             </Button>
         </Form>
     )
