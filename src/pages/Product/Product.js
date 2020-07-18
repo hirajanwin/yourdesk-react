7241260a7c2198669a46b41182f499d29232dda5
Jonathan Cai <caimjonathan@gmail.com>
2020-07-18 15:29:33 -0700

    styling for product page

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -21,41 +21,37 @@ export default function Desk(props) {
     let count = 0;
 
     return (
-        <div className="Body">
-            <div className="DeskBody">
-                {
-                    product &&
-                    <div style={{ display: "flex", flexDirection: "row" }}>
-                        <Card style={{ padding: 20, width: "30vw", height: "100%" }}>
-                            <h5>{product.title}</h5>
-                            <p>ASIN: {product.asin}</p>
-                            <p>Amazon Rating: &nbsp;
+        <div className="DeskBody">
+            {
+                product &&
+                <div className="NewBody">
+                    <Card style={{ padding: 20, margin: 20, height: "100%" }}>
+                        <h5>{product.title}</h5>
+                        <p>ASIN: {product.asin}</p>
+                        <p>Amazon Rating: &nbsp;
                                 <StarRatings
-                                    rating={product.rating}
-                                    starRatedColor="orange"
-                                    numberOfStars={5}
-                                    name='rating'
-                                    starDimension={20}
-                                    starSpacing={1}
-                                />
-                            </p>
-                            <p>ID: {product._id}</p>
-                            <p>Price: ${product.prices[0] ? product.prices[0].value : 0}</p>
-                            <div>
-                                <Button href={product.link} target="_blank" variant="success">Buy</Button>
-                            </div>
-                        </Card>
+                                rating={product.rating}
+                                starRatedColor="orange"
+                                numberOfStars={5}
+                                name='rating'
+                                starDimension={20}
+                                starSpacing={1}
+                            />
+                        </p>
+                        <p>ID: {product._id}</p>
+                        <p>Price: ${product.prices[0] ? product.prices[0].value : 0}</p>
                         <div>
-                            <Image style={{ width: 400, padding: 20 }} src={product.image} />
+                            <Button href={product.link} target="_blank" variant="success">Buy</Button>
                         </div>
-                        <div>
-                            <Card style={{ padding: 20, width: "30vw" }}>
-                                <b>As seen in {count} desks:</b>
-                            </Card>
-                        </div>
+                    </Card>
+                    <div>
+                        <Image style={{ width: 350, padding: 20 }} src={product.image} />
                     </div>
-                }
-            </div>
+                    <Card style={{ padding: 20, margin: 20 }}>
+                        <b>{count > 0 ? `As seen in ${count} desks`: "Not found in any desks!"}</b>
+                    </Card>
+                </div>
+            }
         </div>
     )
 }
