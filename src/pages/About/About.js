8f2b5442301f742525bb920d5c86b6aa033086b6
Jonathan Cai <caimjonathan@gmail.com>
2020-07-28 23:16:23 -0700

    about page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -11,21 +11,24 @@ export default function About() {
                 <p>99% of professional/amateur gamers have a home setup,</p>
                 <p>95% of students are attending school remote,</p>
                 <p>and 5% of companies allow for fully remote work.</p>
-                <p>100% of the above statements are complete guesses, but I'd imagine its somewhere close to reality. Don't quote me.</p>
-                <p>The point is, we are spending a lot of time at our desks these days.</p>
+                <p><b>100% of the above statements are complete guesses</b> <span role="img" aria-label="laughing-emoji">😂</span>
+                &nbsp;, but I'd imagine its somewhere close to reality. Don't quote me.</p>
+                <p>Point is, we are spending a lot of time at our desks these days.</p>
                 <p>There are many reasons to optimize our desk setup - health/ergonomics, productivity, performance, to name a few.</p>
-                <p>There are thousands of sites that give product recommendations, but they're not as genuine as hearing 
+                <p>While there are thousands of sites that give product recommendations, they're not as genuine as hearing
                     about a product from your friend, or from somone who has actually used the product for a while.</p>
                 <p>It's why <a href="https://yourstack.com/">YourStack</a> is so cool.</p>
                 <p>There are many similar sites to this one: <a href="https://deskhunt.com/">DeskHunt</a>, <a href="https://mysetup.co/">MySetup</a>,
                     <a href="https://kit.co/"> Kit.co</a>.</p>
                 <p>That being said, I'm still not quite sure what the differentiating factor for this site will be haha.</p>
-                <br/>
-                <p>Any concerns/questions can be directed to me at caimjonathan(at)gmail.com :)</p>
-                <p>Code is here: <a href="https://github.com/jonathancai11/yourdesk-graphql">backend</a> 
-                    &nbsp; and <a href="https://github.com/jonathancai11/yourdesk-react">frontend</a>.</p>
+                <br />
+                <br />
+                <p>Any concerns/questions can be directed to me at caimjonathan(at)gmail.com <span role="img" aria-label="laughing-emoji">👋</span></p>
+                <p>For more about me, visit <a href="https://jonathancai.com/">jonathancai.com</a>.</p>
+                <p>The code for the website is here: <a href="https://github.com/jonathancai11/yourdesk-graphql">backend</a>
+                    &nbsp;and <a href="https://github.com/jonathancai11/yourdesk-react">frontend</a>.</p>
 
-                <p>Home page graphic from <a href="https://www.vecteezy.com/free-vector/man-computer">Man Computer Vectors by Vecteezy</a>.</p>
+                <p>Home page graphic is from <a href="https://www.vecteezy.com/free-vector/man-computer">Man Computer Vectors by Vecteezy</a>.</p>
 
             </div>
         </div>
